c985594573f0793334df8292fa0fbef201e967b8
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-08-23 12:41:30 +0500

    Update TimeConvertor.java

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,10 +1,6 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public static void main(String[] args) {
-        TimeConvertor object = new TimeConvertor();
-        object.convert(3.5f);
-    }
     public void convert(float minutes) {
         System.out.println((int)minutes * 60);
     }
